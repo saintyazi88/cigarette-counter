7eae44ec676f297fee3c3333f719c6667ce3827d
saintyazi88 <saintyazi@gmail.com>
2025-04-16 18:47:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const db = require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 // Middleware
 app.use(bodyParser.json());
