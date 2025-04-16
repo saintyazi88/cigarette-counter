603f353a602560b7bc3bdf341357f6d8dbd87345
saintyazi88 <saintyazi@gmail.com>
2025-04-16 18:46:27 -0400

    Update db.js

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const { Pool } = require('pg');
 // Create a PostgreSQL connection pool
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 // Test database connection
